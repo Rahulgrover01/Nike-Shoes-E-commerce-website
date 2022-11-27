87f66677c1d5ed959c29b1204e700b20fb777323
Rahul <rahulgrover4841@gmail.com>
2022-11-27 20:55:35 +0530

    added some more design

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const wrapper= document.querySelector(".sliderwrapper")
+// const slideritem= document.querySelector(".slideritem")
 const menuItems= document.querySelectorAll(".menuItem")
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", ()=>{
         wrapper.style.transform= `translateX(${-100 * index}vw)`;
+        // slideritem.style.transform= `translateX(${-100 * index}vw)`;
     });
 });
